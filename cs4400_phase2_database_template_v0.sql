1560d6573198425b9c4179777d91ed90e0a6e1dc
Namkhang Le <Namkhangnle@hotmail.com>
2024-02-22 18:55:06 -0500

    Add files via upload

---
+++ cs4400_phase2_database_template_v0.sql
@@ -0,0 +1,33 @@
+-- CS4400: Introduction to Database Systems (Spring 2024)
+-- Phase II: Create Table & Insert Statements [v0] Monday, February 19, 2024 @ 12:00am EST
+
+-- Team __
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+
+-- Directions:
+-- Please follow all instructions for Phase II as listed on Canvas.
+-- Fill in the team number and names and GT usernames for all members above.
+-- Create Table statements must be manually written, not taken from an SQL Dump file.
+-- This file must run without error for credit.
+
+/* This is a standard preamble for most of our scripts.  The intent is to establish
+a consistent environment for the database behavior. */
+set global transaction isolation level serializable;
+set global SQL_MODE = 'ANSI,TRADITIONAL';
+set names utf8mb4;
+set SQL_SAFE_UPDATES = 0;
+
+set @thisDatabase = 'drone_dispatch';
+drop database if exists drone_dispatch;
+create database if not exists drone_dispatch;
+use drone_dispatch;
+
+-- Define the database structures
+/* You must enter your tables definitions, along with your primary, unique and foreign key
+declarations, and data insertion statements here.  You may sequence them in any order that
+works for you.  When executed, your statements must create a functional database that contains
+all of the data, and supports as many of the constraints as reasonably possible. */
