0273f1c5422654cb291bc5cec026c0cbcc9ce902
Namkhang Le <Namkhangnle@hotmail.com>
2024-04-26 13:13:07 -0400

    Multipage Application Successfully Implemented

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Pilots from './Pilots';
+import Customers from './Customers';
+import Products from './Products';
+import Orders from './Orders';
+import Drones from './Drones';
+import Views from './Views';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/pilots" element={<Pilots />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/drones" element={<Drones />} />
+        <Route path="/views" element={<Views />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
