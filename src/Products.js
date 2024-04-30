08e2420a8a77c3e3bb4c61d247e56fb8f4e4d2a3
Namkhang Le <Namkhangnle@hotmail.com>
2024-04-30 11:03:13 -0400

    Add product data fetching and removal functionality in Products.js

--- src/Products.js
+++ src/Products.js
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { useNavigate } from 'react-router-dom';
 
 function Products() {
   const navigate = useNavigate();
+  const [productData, setProductData] = useState([]);
+  const [barcode, setBarcode] = useState('');
+
+  useEffect(() => {
+    getProductInformation().then(result => setProductData(result));
+  }, []);
+
   return (
     <div>
       <h1>Products Page</h1>
       <input type="text" id="barcode" placeholder="Enter Barcode"></input>
       <input type="text" id="name" placeholder="Enter Product Name"></input>
       <input type="text" id="weight" placeholder="Enter Weight"></input>
+
       <button className="add-button button" onClick={() => addProduct()}>Add Products</button>
-      <button className="cancel-button" onClick={() => removeProduct()}>Remove Products</button>
+      <select className="select-dropdown" onChange={e => setBarcode(e.target.value)}>
+        {productData.map((product, index) => (
+          <option key={index} value={product.barcode}>{product.barcode}</option>
+        ))}
+      </select>
+      <button className="cancel-button" onClick={() => removeProduct(barcode)}>Remove Products</button>
       <button className="back-button" onClick={() => navigate(-1)}>Go Back</button>
     </div>
   );
+}
+
+function getProductInformation() {
+  
+  return fetch('http://localhost:5000/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+      body: JSON.stringify({ sql: 'select distinct barcode from products'}),
+  })
+    .then(res => res.json())
+    .catch(err => console.error(err));
+}
+
+function removeProduct(barcode) {
+  const inputs = [];
+  inputs.push(barcode)
+  fetch('http://localhost:5000/procedure', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ sql: 'call remove_product(?)',
+                           parameters: inputs}),
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
 }
 
 function addProduct() {
@@ -37,18 +79,6 @@ function addProduct() {
     .catch(err => console.error(err));
 }
 
-function removeProduct() {
-  fetch('http://localhost:5000/procedure', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ sql: 'call remove_product(?)',
-                           parameters: ['cjordan5']}),
-  })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
-}
+
 
 export default Products;
